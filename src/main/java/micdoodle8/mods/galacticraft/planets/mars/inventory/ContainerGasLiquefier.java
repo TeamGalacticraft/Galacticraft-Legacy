7b35c1fd299239273508e2c9ae6505bbb94067bd
Klairm <ivangardis@hotmail.com>
2022-08-16 15:58:58 +0200

    fix[ContainerGasLiquefier]: ItemStack array sizes was for some reason hardcoded, so I used a method to get their size instead

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/inventory/ContainerGasLiquefier.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/inventory/ContainerGasLiquefier.java
@@ -14,140 +14,114 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
-public class ContainerGasLiquefier extends Container
-{
+public class ContainerGasLiquefier extends Container {
 
-    private final TileEntityGasLiquefier tileEntity;
+	private final TileEntityGasLiquefier tileEntity;
 
-    public ContainerGasLiquefier(InventoryPlayer par1InventoryPlayer, TileEntityGasLiquefier tileEntity, EntityPlayer player)
-    {
-        this.tileEntity = tileEntity;
+	public ContainerGasLiquefier(InventoryPlayer par1InventoryPlayer, TileEntityGasLiquefier tileEntity, EntityPlayer player) {
+		this.tileEntity = tileEntity;
 
-        // Electric Input Slot
-        this.addSlotToContainer(new SlotSpecific(tileEntity, 0, 34, 50, IItemElectric.class));
+		// Electric Input Slot
+		this.addSlotToContainer(new SlotSpecific(tileEntity, 0, 34, 50, IItemElectric.class));
 
-        // Input slot
-        this.addSlotToContainer(new Slot(tileEntity, 1, 7, 7));
+		// Input slot
+		this.addSlotToContainer(new Slot(tileEntity, 1, 7, 7));
 
-        // 2 output slots
-        this.addSlotToContainer(new Slot(tileEntity, 2, 132, 7));
+		// 2 output slots
+		this.addSlotToContainer(new Slot(tileEntity, 2, 132, 7));
 
-        int var3;
+		int var3;
 
-        for (var3 = 0; var3 < 3; ++var3)
-        {
-            for (int var4 = 0; var4 < 9; ++var4)
-            {
-                this.addSlotToContainer(new Slot(par1InventoryPlayer, var4 + var3 * 9 + 9, 8 + var4 * 18, 104 + var3 * 18 - 18));
-            }
-        }
+		for (var3 = 0; var3 < 3; ++var3) {
+			for (int var4 = 0; var4 < 9; ++var4) {
+				this.addSlotToContainer(new Slot(par1InventoryPlayer, var4 + var3 * 9 + 9, 8 + var4 * 18, 104 + var3 * 18 - 18));
+			}
+		}
 
-        for (var3 = 0; var3 < 9; ++var3)
-        {
-            this.addSlotToContainer(new Slot(par1InventoryPlayer, var3, 8 + var3 * 18, 144));
-        }
+		for (var3 = 0; var3 < 9; ++var3) {
+			this.addSlotToContainer(new Slot(par1InventoryPlayer, var3, 8 + var3 * 18, 144));
+		}
 
-        tileEntity.openInventory(player);
-    }
+		tileEntity.openInventory(player);
+	}
 
-    @Override
-    public void onContainerClosed(EntityPlayer entityplayer)
-    {
-        super.onContainerClosed(entityplayer);
-        this.tileEntity.closeInventory(entityplayer);
-    }
+	@Override
+	public void onContainerClosed(EntityPlayer entityplayer) {
+		super.onContainerClosed(entityplayer);
+		this.tileEntity.closeInventory(entityplayer);
+	}
 
-    @Override
-    public boolean canInteractWith(EntityPlayer par1EntityPlayer)
-    {
-        return this.tileEntity.isUsableByPlayer(par1EntityPlayer);
-    }
+	@Override
+	public boolean canInteractWith(EntityPlayer par1EntityPlayer) {
+		return this.tileEntity.isUsableByPlayer(par1EntityPlayer);
+	}
 
-    /**
-     * Called to transfer a stack from one inventory to the other eg. when shift
-     * clicking.
-     */
-    @Override
-    public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int par1)
-    {
-        ItemStack itemStack1 = ItemStack.EMPTY;
-        final Slot slot = this.inventorySlots.get(par1);
+	/**
+	 * Called to transfer a stack from one inventory to the other eg. when shift
+	 * clicking.
+	 */
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int par1) {
+		ItemStack itemStack1 = ItemStack.EMPTY;
+		final Slot slot = this.inventorySlots.get(par1);
 
-        if (slot != null && slot.getHasStack())
-        {
-            final ItemStack var4 = slot.getStack();
-            itemStack1 = var4.copy();
+		if (slot != null && slot.getHasStack()) {
+			final ItemStack var4 = slot.getStack();
+			itemStack1 = var4.copy();
 
-            if (par1 < this.tileEntity.getSizeInventory())
-            {
+			if (par1 < this.tileEntity.getSizeInventory()) {
 
-                if (!this.mergeItemStack(var4, this.tileEntity.getSizeInventory(), this.inventorySlots.size(), true))
-                {
-                    return ItemStack.EMPTY;
-                }
+				if (!this.mergeItemStack(var4, this.tileEntity.getSizeInventory(), this.inventorySlots.size(), true)) {
+					return ItemStack.EMPTY;
+				}
 
-                if (par1 == 2)
-                {
-                    slot.onSlotChange(var4, itemStack1);
-                }
-            } else
-            {
-                if (EnergyUtil.isElectricItem(var4.getItem()))
-                {
-                    if (!this.mergeItemStack(var4, 0, this.tileEntity.getSizeInventory(), false))
-                    {
-                        return ItemStack.EMPTY;
-                    }
-                } else
-                {
-                    boolean outputTankSlotsSuccess = false;
+				if (par1 == 2) {
+					slot.onSlotChange(var4, itemStack1);
+				}
+			} else {
+				if (EnergyUtil.isElectricItem(var4.getItem())) {
+					if (!this.mergeItemStack(var4, 0, this.tileEntity.getSizeInventory(), false)) {
+						return ItemStack.EMPTY;
+					}
+				} else {
+					boolean outputTankSlotsSuccess = false;
 
-                    if (!outputTankSlotsSuccess && FluidUtil.isEmptyContainerFor(var4, this.tileEntity.liquidTank.getFluid()))
-                    {
-                        if (this.mergeItemStack(var4, 2, 3, false))
-                        {
-                            outputTankSlotsSuccess = true;
-                        }
-                    }
+					if (!outputTankSlotsSuccess && FluidUtil.isEmptyContainerFor(var4, this.tileEntity.liquidTank.getFluid())) {
+						if (this.mergeItemStack(var4, 2, 3, false)) {
+							outputTankSlotsSuccess = true;
+						}
+					}
 
-                    if (!outputTankSlotsSuccess)
-                    {
-                        if (FluidUtil.isFilledContainer(var4) || var4.getItem() instanceof ItemAtmosphericValve)
-                        {
-                            if (!this.mergeItemStack(var4, 1, 2, false))
-                            {
-                                return ItemStack.EMPTY;
-                            }
-                        } else if (par1 < 31)
-                        {
-                            if (!this.mergeItemStack(var4, 31, 40, false))
-                            {
-                                return ItemStack.EMPTY;
-                            }
-                        } else if (!this.mergeItemStack(var4, 4, 31, false))
-                        {
-                            return ItemStack.EMPTY;
-                        }
-                    }
-                }
-            }
+					if (!outputTankSlotsSuccess) {
+						if (FluidUtil.isFilledContainer(var4) || var4.getItem() instanceof ItemAtmosphericValve) {
+							if (!this.mergeItemStack(var4, 1, 2, false)) {
+								return ItemStack.EMPTY;
+							}
+						} else if (par1 < 31) {
 
-            if (var4.isEmpty())
-            {
-                slot.putStack(ItemStack.EMPTY);
-            } else
-            {
-                slot.onSlotChanged();
-            }
+							if (!this.mergeItemStack(var4, 31, 39, false)) {
+								return ItemStack.EMPTY;
+							}
+						} else if (!this.mergeItemStack(var4, 4, 31, false)) {
+							return ItemStack.EMPTY;
+						}
+					}
+				}
+			}
 
-            if (var4.getCount() == itemStack1.getCount())
-            {
-                return ItemStack.EMPTY;
-            }
+			if (var4.isEmpty()) {
+				slot.putStack(ItemStack.EMPTY);
+			} else {
+				slot.onSlotChanged();
+			}
 
-            slot.onTake(par1EntityPlayer, var4);
-        }
+			if (var4.getCount() == itemStack1.getCount()) {
+				return ItemStack.EMPTY;
+			}
 
-        return itemStack1;
-    }
+			slot.onTake(par1EntityPlayer, var4);
+		}
+
+		return itemStack1;
+	}
 }
